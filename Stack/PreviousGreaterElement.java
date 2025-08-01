5106587d3c0a0546ebf34ae303ac283f74aa10ab
Harshit-TheCoder <harshitharlalka11@gmail.com>
2025-08-01 19:17:13 +0530

    more added stacks

---
+++ Stack/PreviousGreaterElement.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.Stack;
+
+public class PreviousGreaterElement {
+    public static int[] prevGreater(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+
+}
